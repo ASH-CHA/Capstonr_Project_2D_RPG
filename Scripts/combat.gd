10bceea65cc7a5909eea020935d2c1c72bb85cdb
ASH-CHA <ashton15chavez@gmail.com>
2025-09-06 03:57:32 +0200

    Added a Defense Button to Combat
    I added a defense button to reduce an enemy attack by half. However, still need to implement a defend animation.

--- Scripts/combat.gd
+++ Scripts/combat.gd
@@ -3,10 +3,12 @@ extends Node2D
 var player_hp = 100
 var enemy_hp = null
 var player_turn = true
+var defending = false  # track if player is defending
 
 @onready var player_health_label = $PlayerHealth
 @onready var enemy_health_label = $EnemyHealth
 @onready var attack_button = $AttackButton
+@onready var defend_button = $DefendButton 
 @onready var combat_log = $CombatLog
 
 @onready var enemy_animated_sprite = $Enemy/AnimatedSprite2D
@@ -15,10 +17,15 @@ var player_turn = true
 func _ready():
 	enemy_animated_sprite.play("idle")
 	player_animated_sprite.play("idle")
+	
+	# Connect buttons
 	attack_button.pressed.connect(_on_attack_pressed)
+	defend_button.pressed.connect(_on_defend_pressed)  # NEW connection
+	
 	enemy_hp = GameManager.enemy_stats[GameManager.last_enemy_type]
 	update_ui()
 
+# ATTACK
 func _on_attack_pressed():
 	if player_turn:
 		player_animated_sprite.play("attack_right")
@@ -27,17 +34,15 @@ func _on_attack_pressed():
 		enemy_hp -= damage
 		combat_log.text = "You deal " + str(damage) + " damage!"
 		
-		
-		
 		if enemy_hp <= 0:
 			combat_log.text = "Victory! Enemy defeated!"
 			attack_button.disabled = true
+			defend_button.disabled = true
 			enemy_animated_sprite.play("death")
 			
 			# Mark enemy defeated
 			GameManager.defeated_enemies[GameManager.last_enemy_id] = true
 			print(GameManager.defeated_enemies)
-			
 			
 			# Return to world after short delay
 			get_tree().create_timer(1.5).timeout.connect(_return_to_world)
@@ -47,31 +52,54 @@ func _on_attack_pressed():
 	
 	update_ui()
 
-	
-func _return_to_world():
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
+# DEFEND
+func _on_defend_pressed():
+	if player_turn:
+		defending = true  # player prepares to defend
+		player_animated_sprite.play("defend") # (insert new anim)
+		combat_log.text = "You brace yourself for the enemy’s attack!"
+		
+		player_turn = false
+		await get_tree().create_timer(1.0).timeout.connect(_enemy_attack)
+
+
+# ENEMY ATTACK
 func _enemy_attack():
 	enemy_animated_sprite.play("idle")
 	enemy_animated_sprite.play("attack_left")
 	player_animated_sprite.play("hit_left")
+	
 	var damage = randi_range(10, 20)
+	if defending:
+		damage = int(damage / 2)  # reduce damage if defending
+		defending = false          # reset defending after effect
+		combat_log.text = "Enemy attacks, but you defend! Damage reduced to " + str(damage) + "!"
+	else:
+		combat_log.text = "Enemy deals " + str(damage) + " damage!"
+	
 	player_hp -= damage
-	combat_log.text = "Enemy deals " + str(damage) + " damage!"
 	
 	if player_hp <= 0:
 		combat_log.text = "Defeat! You have fallen!"
 		attack_button.disabled = true
+		defend_button.disabled = true
 		get_tree().create_timer(1.5).timeout.connect(_game_over)
 	else:
 		player_turn = true
 	
 	update_ui()
 
+
+# RETURN / GAME OVER
+func _return_to_world():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
 func _game_over():
 	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
 
 
+# UI UPDATE
 func update_ui():
 	player_health_label.text = "Player HP: " + str(player_hp)
 	enemy_health_label.text = "Enemy HP: " + str(enemy_hp)
