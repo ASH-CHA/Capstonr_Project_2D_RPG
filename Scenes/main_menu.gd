7670ae22f92a2a981603df47b8b24f94a89d2961
ASH-CHA <ashton15chavez@gmail.com>
2025-09-16 13:39:38 +0200

    Added Dialogue to the NPC
    I was able to add dialogue. By pressing the 'c' key the dialogue box will pop up. Continue the dialogue by pressing the space bar or enter key.
    
    After finishing the dialogue the NPC will resume roaming around.
    
    Now I just have to figure out how to make it modular so we have a variety NPCs with different dialogue.
    
    I also somewhat fixed the collision problem. The NPC now collides with the same objects as the player. However, both are still able to roam/pass through objects in other regions or off the map.

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,9 +1,19 @@
 extends Control
 
+var button_type = null
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	#get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	button_type = "Start Game"
+	$fade_transition.show()
+	$fade_transition/fade_timer.start()
+	$fade_transition/AnimationPlayer.play("fade_in")
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_fade_timer_timeout() -> void:
+	if button_type == "Start Game":
+		get_tree().change_scene_to_file("res://Scenes/main.tscn")
