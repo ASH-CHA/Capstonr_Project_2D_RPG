f6d4915852474c6623a1ae418b6827df82a48f7d
Zrider8019 <Zrider8019@gmail.com>
2025-09-19 20:33:42 -0400

    Revert "Merge branch 'main' of https://github.com/ASH-CHA/Capstonr_Project_2D_RPG"
    This reverts commit ef499c550a7eba0a611f620f45c001773f3215ea, reversing
    changes made to b20aec3bacced6131f06afc9a9f1287b044607dd.

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,19 +1,9 @@
 extends Control
 
-var button_type = null
 
 func _on_start_pressed() -> void:
-	#get_tree().change_scene_to_file("res://Scenes/main.tscn")
-	button_type = "Start Game"
-	$fade_transition.show()
-	$fade_transition/fade_timer.start()
-	$fade_transition/AnimationPlayer.play("fade_in")
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_fade_timer_timeout() -> void:
-	if button_type == "Start Game":
-		get_tree().change_scene_to_file("res://Scenes/main.tscn")
--- Scripts/after_combat.gd
+++ Scripts/after_combat.gd
@@ -1,5 +1,4 @@
 extends Node
 
 func _ready():
-	$fade_transition/AnimationPlayer.play("fade_out")
 	$Player.global_position = GameManager.player_position
