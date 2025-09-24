7bf8cb94d81b0dfda2807cd717cfa37ccd226548
ASH-CHA <ashton15chavez@gmail.com>
2025-09-24 16:04:20 +0200

    Finished tutorial and added NPCs for Desert and Corruption Region
    I think I finalized all the dialogue with the NPCs, you can check the .json files if you think there can be improvements.
    
    I added an NPC for the Desert Region to further the plot and introduce the health potion, which will later be add around the Desert region for the player to collect.
    
    I also added a survivor NPC for the Corruption Region to further the plot and introduce the herb item, which will later be add around the Corruption region for the player to collect.

---
+++ Scripts/survivor_npc.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+var is_chatting = false
+
+var player
+var player_in_chat_zone = false
+
+func _ready():
+	pass
+
+func _process(_delta):
+	$AnimatedSprite2D.play("idle_left")
+	
+	# Press the 'c' key to activate when near chat zone
+	if player_in_chat_zone and Input.is_action_just_pressed("chat"):
+		print("chatting with npc")
+		$survivor_npc_dialogue.start()
+		is_chatting = true
+		$AnimatedSprite2D.play("idle")
+
+func choose(array):
+	array.shuffle()
+	return array.front()
+
+func move(_delta):
+	if !is_chatting:
+		$AnimatedSprite2D.play("idle_left")
+
+func _on_chat_detection_area_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player = body
+		player_in_chat_zone = true
+
+func _on_chat_detection_area_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_chat_zone = false
+
+
+func _on_timer_timeout() -> void:
+	$Timer.wait_time = choose([0.5, 1, 1.5])
+
+
+func _on_survivor_npc_dialogue_dialogue_finished() -> void:
+	is_chatting = false
