e0383e7a238a4b6a49eacee924c46313caabf299
Zrider8019 <Zrider8019@gmail.com>
2025-09-24 20:27:30 -0400

    Game over shows Tutorial
    The game over scene now correctly displays the intro text when the game is reloaded.

--- Scenes/game_over.gd
+++ Scenes/game_over.gd
@@ -3,8 +3,9 @@ extends Control
 func _on_start_pressed() -> void:
 	GameManager.player_position = GameManager.player_start_position
 	GameManager.player_hp = GameManager.player_max_hp
-	GameManager.defeated_enemies.clear()
-	GameManager.enemy_current_hp.clear()
+	GameManager.defeated_enemies.clear() #Restores Defeated Enemies to the map
+	GameManager.enemy_current_hp.clear() #If enemies were damage, they are restored to full health
+	GameManager.intro_shown = false
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_exit_pressed() -> void:
