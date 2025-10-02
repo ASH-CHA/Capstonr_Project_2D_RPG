7df1f92dc6c72d9777dd65113b463edd99873e6a
ASH-CHA <ashton15chavez@gmail.com>
2025-10-02 14:59:35 +0200

    Spreaded enemies across map / Added back fade transition
    I added all enemies in their respective regions.
    
    I also added back the fade transition from the Main Menu to the gameplay.

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,9 +1,20 @@
 extends Control
 
+var button_type = null
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+func _on_button_pressed() -> void:
+	button_type = "Start Game"
+	$fade_transition.show()
+	$fade_transition/fade_timer.start();
+	$fade_transition/AnimationPlayer.play("fade_in")
+	
+	#get_tree().change_scene_to_file("res://Scenes/main.tscn")
 	GameManager.player_start_position = GameManager.player_position
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_fade_timer_timeout() -> void:
+	if button_type == "Start Game":
+		get_tree().change_scene_to_file("res://Scenes/main.tscn")
